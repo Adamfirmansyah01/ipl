b8bd46acde521ed7125e3a62758eb5c6be2cdca3
DESKTOP-3I9V9Q6\asus <adamfirmansyah479@gmail.com>
2023-10-18 19:05:13 +0700

    perubahan sesuai prinsip open close

--- openclose/StandardCinema.java
+++ openclose/StandardCinema.java
@@ -4,5 +4,9 @@ public class StandardCinema extends Cinema {
     public StandardCinema(double price){
         this.price = price;
     }
+    @Override
+    Double calculateAdminFee(){
+        return this.price * 10/100;    
+    }
     
 }
