c3784d21925a3bfd92d246cf4a5da952458d530c
DESKTOP-3I9V9Q6\asus <adamfirmansyah479@gmail.com>
2023-10-18 11:35:12 +0700

    ubah sesuai prinsip responsibility

--- srp/Order.java
+++ srp/Order.java
@@ -3,26 +3,18 @@ package srp;
 class Order {
     void calculateTotalSum() {
         /*...*/
-            }
-            void getItems() {
+    }
+    void getItems() {
         /*...*/
-            }
+    }
     void getItemCount() {
         /*...*/
-            }
+    }
     void addItem(Item item) {
         /*...*/
-            }
+    }
     void deleteItem(Item item) {
         /*...*/
-            }
-    void printOrder() {
-        /*...*/
-            }
-    void showOrder() {
-        /*...*/
-            }
+    }
 
-    void getDailyHistory() {}
-    void getMonthlyHistory() {}
 }
