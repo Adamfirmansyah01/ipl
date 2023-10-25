bb2096973648e30c6ea8d652719757c4bf52427c
DESKTOP-3I9V9Q6\asus <adamfirmansyah479@gmail.com>
2023-10-25 14:58:39 +0700

    Perubahan dpi

---
+++ dpi/Main.java
@@ -0,0 +1,15 @@
+package dpi;
+
+public class Main {
+    public static void main (String[] args){
+        Car fuelcar = new Car(new PetrolEngine());
+        Car dieselCar = new Car(new DieselEngine());
+        Car hybridCar = new Car(new HybridEngine());
+
+        //Jalankan mobil
+        fuelcar.start();
+        dieselCar.start();
+        hybridCar.start();
+    }
+
+}
